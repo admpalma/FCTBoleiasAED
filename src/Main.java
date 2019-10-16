e368d0d7f4f7a9136816d581c6d5b12e5a23b1b2
André Palma <andremarquespalma@gmail.com>
2019-10-16 20:34:16 +0100

    Enum refactor and Code cleanup

--- src/Main.java
+++ src/Main.java
@@ -55,46 +55,129 @@ public class Main {
 	private static final String FINAL_MESSAGE = "Obrigado. Ate a proxima.";
 
 	/**
-	 * Enum for user commands Each command is associated with its message in the
-	 * help menu
-	 *
-	 * @author Joao Bordalo
-	 *
+	 * {@link Enum} containing all of the possible commands
+	 * and warning messages where applicable.
 	 */
-	private enum GlobalCommand {
-		TERMINA("termina - Termina a execucao do programa"),
-		REGISTA("regista - Regista um novo utilizador no programa"),
-		ENTRADA("entrada - Permite a entrada (\"login\") dum utilizador no programa"),
-		AJUDA("ajuda - Mostra os comandos existentes"), SAI("sai - Termina a sessao deste utilizador no programa"),
-		UNKNOWN("");
+	private enum Commands {
+		AJUDA, TERMINA, REGISTA, ENTRADA, SAI, NOVA, LISTA, BOLEIA, CONSULTA, REMOVE, RETIRA, UNKNOWN("Comando invalido.");
 
-		private final String helpMessage; // Associates each command with its help message
+		/**
+		 * Stores each {@link Commands command's} warning message (where applicable)
+		 */
+		private final String warningMessage;
 
-		private GlobalCommand(String helpMessage) {
-			this.helpMessage = helpMessage;
+		private Commands() {
+			this.warningMessage = null;
 		}
 
-		public String getHelpMessage() {
-			return helpMessage;
+		private Commands(String warningMessage) {
+			this.warningMessage = warningMessage;
+		}
+
+		/**
+		 * @return the warningMessage
+		 */
+		public String getWarningMessage() {
+			return warningMessage;
+		}
+
+		/**
+		 * Converts the user's input into a {@link Commands command}
+		 *
+		 * @param in - {@link Scanner} containing the user's input
+		 * @return the matching {@link Command command} to the user's input,
+		 * {@link Commands.UNKNOWN} in case there's no other valid match
+		 */
+		private static Commands getCommand(Scanner in) {
+			try {
+				assert(in.hasNextLine());
+				String command = in.nextLine().toUpperCase();
+				return Commands.valueOf(command);
+			} catch (IllegalArgumentException e) {
+				return Commands.UNKNOWN;
+			}
 		}
 
 	}
 
-	private enum LoggedInCommand {
-		NOVA("nova - Regista uma nova deslocacao"), LISTA("lista - Lista todas ou algumas deslocacoes registadas"),
-		BOLEIA("boleia - Regista uma boleia para uma dada deslocacao"),
-		CONSULTA("consulta - Lista a informacao de uma dada deslocacao"), REMOVE("remove - Retira uma dada deslocacao"),
-		AJUDA("ajuda - Mostra os comandos existentes"), SAI("sai - Termina a sessao deste utilizador no programa"),
-		UNKNOWN("");
+	/**
+	 * {@link Enum} containing the commands that can be executed
+	 * when there is no {@link User} logged in and its corresponding help messages.
+	 */
+	private enum LoggedOutCommands {
+		AJUDA("ajuda - Mostra os comandos existentes"),
+		TERMINA("termina - Termina a execucao do programa"),
+		REGISTA("regista - Regista um novo utilizador no programa"),
+		ENTRADA("entrada - Permite a entrada (\"login\") dum utilizador no programa");
 
-		private final String helpMessage; // Associates each command with its help message
+		/**
+		 * Stores each {@link LoggedOutCommands command's} help message
+		 */
+		private final String helpMessage;
 
-		private LoggedInCommand(String helpMessage) {
+		private LoggedOutCommands(String helpMessage) {
 			this.helpMessage = helpMessage;
 		}
 
+		/**
+		 * @return this {@link LoggedOutCommands command´s} the help message
+		 */
 		public String getHelpMessage() {
 			return helpMessage;
+		}
+
+		/**
+		 * Converts a {@link Commands} type command into {@link LoggedOutCommands}
+		 * if such is possible, otherwise returns {@link IllegalArgumentException}
+		 * @param command {@link Commands} type to be converted
+		 * @return the converted command
+		 * @throws IllegalArgumentException if it is not possible to convert the given command
+		 */
+		public static LoggedOutCommands toLoggedOutCommand(Commands command) throws IllegalArgumentException {
+			return LoggedOutCommands.valueOf(command.name());
+		}
+
+	}
+
+	/**
+	 * {@link Enum} containing the commands that can be executed
+	 * when there is a {@link User} logged in and its corresponding help messages.
+	 */
+	private enum LoggedInCommands {
+		AJUDA("ajuda - Mostra os comandos existentes"),
+		SAI("sai - Termina a sessao deste utilizador no programa"),
+		NOVA("nova - Regista uma nova deslocacao"),
+		LISTA("lista - Lista todas ou algumas deslocacoes registadas"),
+		BOLEIA("consulta - Lista a informacao de uma dada deslocacao"),
+		CONSULTA("consulta - Lista a informacao de uma dada deslocacao"),
+		RETIRA("retira - Retira uma dada boleia"),
+		REMOVE("remove - Retira uma dada deslocacao");
+
+		/**
+		 * Stores each {@link LoggedInCommands command's} help message
+		 */
+		private final String helpMessage;
+
+		private LoggedInCommands(String helpMessage) {
+			this.helpMessage = helpMessage;
+		}
+
+		/**
+		 * @return this {@link LoggedInCommands command´s} the help message
+		 */
+		public String getHelpMessage() {
+			return helpMessage;
+		}
+
+		/**
+		 * Converts a {@link Commands} type command into {@link LoggedInCommands}
+		 * if such is possible, otherwise returns {@link IllegalArgumentException}
+		 * @param command {@link Commands} type to be converted
+		 * @return the converted command
+		 * @throws IllegalArgumentException if it is not possible to convert the given command
+		 */
+		public static LoggedInCommands toLoggedInCommand(Commands command) throws IllegalArgumentException {
+			return LoggedInCommands.valueOf(command.name());
 		}
 
 	}
@@ -102,173 +185,160 @@ public class Main {
 	// MENU RELATED METHODS
 
 	/**
-	 * main method containing the essential local variables for the program
-	 * execution It is also responsible for selecting how to process the user's
-	 * input
+	 * Starts the execution of the program FCTBoleias,
+	 * resuming its status from the previous execution
+	 * and saves it when the user chooses to exit.
 	 */
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		Manager manager = new ManagerClass();
-
-		GlobalCommand cmd = getGlobalCommand(in);
-		
-		// TODO Fix with enum thing
-		
+		Commands command;
 		do {
-
+			// TODO prompt shit
+			//System.out.printf("%s %s", manager.getCurrentUserEmail(), DEFAULT_PROMPT);
+			command = Commands.getCommand(in);
 			if (manager.isLoggedIn()) {
-				loggedInCommandInterpreter(manager, in);
+				executeLoggedInCommand(command, manager, in);
 			} else {
-				globalCommandInterpreter(manager, in);
+				executeLoggedOutCommand(command, manager, in);
 			}
-
-		} while (manager.isLoggedIn() || !(cmd.equals(GlobalCommand.TERMINA)));
-
+		} while (manager.isLoggedIn() || !(command.equals(Commands.TERMINA)));
 		in.close();
+	}
+
+	/**
+	 * Attempts to execute the given {@link Commands command} assumimng there is no user logged in,
+	 * and prints a warning in case this attempt fails.
+	 * @param command {@link Commands Command} to be executed
+	 * @param manager {@link Manager} containing the most relevant data of the program
+	 * @param in {@link Scanner} that might contain adicional user input
+	 */
+	private static void executeLoggedOutCommand(Commands command, Manager manager, Scanner in) {
+		try {
+			assert(manager.isLoggedIn());
+			LoggedOutCommands loggedOutCommand = LoggedOutCommands.toLoggedOutCommand(command);
+			loggedOutCommandInterpreter(manager, in, loggedOutCommand);
+		} catch (IllegalArgumentException e) {
+			unknownCommand(in);
+		}
 	}
 
 	// NOT LOGGED IN METHODS
 
 	/**
 	 * Command interpreter when no user is logged in
-	 * 
+	 * TODO
 	 * @param manager - manager object
 	 * @param in      - scanner to get user input
+	 * @param loggedOutCommand
 	 */
-	private static void globalCommandInterpreter(Manager manager, Scanner in) {
-		GlobalCommand cmd = getGlobalCommand(in);
-		switch (cmd) {
+	private static void loggedOutCommandInterpreter(Manager manager, Scanner in, LoggedOutCommands loggedOutCommand) {
+		switch (loggedOutCommand) {
 		case AJUDA:
-			globalProcessHelp();
-			break;
-		case TERMINA:
-			processEnd();
-			break;
-		case REGISTA:
-			processRegister(manager, in);
+			loggedOutHelp(manager);
 			break;
 		case ENTRADA:
-			processLogin(manager, in);
 			break;
-		default:
-			processUnknownCommand(in);
+		case REGISTA:
+			break;
+		case TERMINA:
+			processEnd(manager);
 			break;
 		}
 	}
 
-	private static void processLogin(Manager manager, Scanner in) {
-		
-		String name = in.nextLine().trim();
-		
-		
-		
-	}
-
-	private static void processRegister(Manager manager, Scanner in) {
-		// TODO Auto-generated method stub
-
-	}
-
 	/**
-	 * Gets a user global command
-	 *
-	 * @param in - scanner
-	 * @return Command.valueOf(command) or Command.UNKNOWN
+	 * Prints the help messages associated with a {@link User} not being logged in
+	 * @param manager TODO
 	 */
-	private static GlobalCommand getGlobalCommand(Scanner in) {
-		try {
-			System.out.printf(DEFAULT_PROMPT);
-			String command = in.nextLine().toUpperCase();
-			return GlobalCommand.valueOf(command);
-		} catch (IllegalArgumentException e) {
-			return GlobalCommand.UNKNOWN;
-		}
-	}
-
-	/**
-	 * Gets a user logged in command
-	 *
-	 * @param in - scanner
-	 * @return Command.valueOf(command) or Command.UNKNOWN
-	 */
-	private static LoggedInCommand getLoggedInCommand(Scanner in, Manager manager) {
-		try {
-			// TODO
-			// System.out.printf("%s %s", manager.getCurrentUserEmail(), DEFAULT_PROMPT);
-			String command = in.nextLine().toUpperCase();
-			return LoggedInCommand.valueOf(command);
-		} catch (IllegalArgumentException e) {
-			return LoggedInCommand.UNKNOWN;
-		}
-	}
-
-	/**
-	 * Prints the help message associated with a user not being logged in
-	 *
-	 * @pre: !manager.isLoggedIn()
-	 */
-	private static void globalProcessHelp() {
-		// If there isn't a user logged in
-		for (GlobalCommand command : GlobalCommand.values()) {
+	private static void loggedOutHelp(Manager manager) {
+		assert(!manager.isLoggedIn());
+		for (LoggedOutCommands command : LoggedOutCommands.values()) {
 			System.out.printf(command.getHelpMessage());
 		}
-
 	}
 
 	/**
 	 * Prints the ending of program message
-	 *
-	 * @pre: !manager.isLoggedIn()
+	 * (should only run when there's no {@link User} logged in)
+	 * @param manager TODO
 	 */
-	private static void processEnd() {
+	private static void processEnd(Manager manager) {
+		assert(!manager.isLoggedIn());
 		System.out.println(FINAL_MESSAGE);
 	}
 
 	// LOGGED IN METHODS
+	
+	/**
+	 * Attempts to execute the given {@link Commands command} assumimng there is no user logged in,
+	 * and prints a warning in case this attempt fails.
+	 * @param command {@link Commands Command} to be executed
+	 * @param manager {@link Manager} containing the most relevant data of the program
+	 * @param in {@link Scanner} that might contain adicional user input
+	 */
+	private static void executeLoggedInCommand(Commands command, Manager manager, Scanner in) {
+		try {
+			assert(manager.isLoggedIn());
+			LoggedInCommands loggedInCommand = LoggedInCommands.toLoggedInCommand(command);
+			loggedInCommandInterpreter(manager, in, loggedInCommand);
+		} catch (IllegalArgumentException e) {
+			unknownCommand(in);
+		}
+	}
 
 	/**
 	 * Command interpreter when there's a user logged in
-	 * 
+	 * TODO
 	 * @param manager - manager object
 	 * @param in      - scanner to get user input
+	 * @param loggedInCommand
 	 *
-	 * @pre: manager.isLoggedIn()
 	 */
-	private static void loggedInCommandInterpreter(Manager manager, Scanner in) {
-		LoggedInCommand cmd = getLoggedInCommand(in, manager);
-		switch (cmd) {
+	private static void loggedInCommandInterpreter(Manager manager, Scanner in, LoggedInCommands loggedInCommand) {
+		switch (loggedInCommand) {
 		case AJUDA:
-			loggedInProcessHelp();
-			break;
-		case SAI:
-			processLeave(manager);
-			break;
-		case NOVA:
-			processNew(manager, in);
-			break;
-		case LISTA:
-			processList(manager, in);
+			loggedInHelp(manager);
 			break;
 		case BOLEIA:
-			processTakeRide(manager, in);
+			takeRide(manager, in);
 			break;
 		case CONSULTA:
-			processConsult(manager, in);
+			break;
+		case LISTA:
+			break;
+		case NOVA:
+			addTrip(manager, in);
 			break;
 		case REMOVE:
-			processRemove(manager, in);
+			remove(manager, in);
 			break;
-		default:
-			processUnknownCommand(in);
+		case RETIRA:
+			break;
+		case SAI:
 			break;
 		}
 	}
 
-	private static void processRemove(Manager manager, Scanner in) {
-		
+	/**
+	 * Prints the help messages associated with a {@link User} being logged in
+	 * @param manager TODO
+	 */
+	private static void loggedInHelp(Manager manager) {
+		assert(manager.isLoggedIn());
+		for (LoggedInCommands command : LoggedInCommands.values()) {
+			System.out.printf(command.getHelpMessage());
+		}
+	}
+
+	/**
+	 * TODO
+	 * @param manager
+	 * @param in
+	 */
+	private static void remove(Manager manager, Scanner in) {
 		String date = in.nextLine();
 		in.nextLine();
-
 		try {
 			manager.remove(date);
 			System.out.println(RIDE_REMOVED);
@@ -277,15 +347,14 @@ public class Main {
 		}
 	}
 
-	private static void processConsult(Manager manager, Scanner in) {
-		// TODO Auto-generated method stub
-
-	}
-
-	private static void processTakeRide(Manager manager, Scanner in) {
+	/**
+	 * TODO
+	 * @param manager
+	 * @param in
+	 */
+	private static void takeRide(Manager manager, Scanner in) {
 		String name = in.next();
 		String date = in.next().trim();
-
 		try {
 			manager.addNewRide(name, date);
 			System.out.println(RIDE_REGISTERED);
@@ -300,22 +369,15 @@ public class Main {
 				e1.printStackTrace();
 			}
 		}
-		
-		
-	}
-
-	private static void processList(Manager manager, Scanner in) {
-		// TODO Auto-generated method stub
-
 	}
 
 	/**
 	 * Registers a new trip on current {@link User}
-	 * 
+	 * TODO
 	 * @param manager - manager class
 	 * @param in      - Scanner
 	 */
-	private static void processNew(Manager manager, Scanner in) {
+	private static void addTrip(Manager manager, Scanner in) {
 		String origin = in.nextLine();
 		String destiny = in.nextLine();
 		String date = in.next();
@@ -323,7 +385,6 @@ public class Main {
 		int duration = in.nextInt();
 		int numberSeats = in.nextInt();
 		in.nextLine();
-
 		try {
 			manager.addNewTrip(origin, destiny, date, hourMinute, duration, numberSeats);
 		} catch (NotLoggedInException | InvalidDataException e) {
@@ -336,36 +397,18 @@ public class Main {
 				e1.printStackTrace();
 			}
 		}
-
-	}
-
-	private static void processLeave(Manager manager) {
-		// TODO Auto-generated method stub
-
-	}
-
-	/**
-	 * Prints the help message associated with a user being logged in
-	 *
-	 * @pre: manager.isLoggedIn()
-	 */
-	private static void loggedInProcessHelp() {
-		// If there is a user logged in
-		for (LoggedInCommand command : LoggedInCommand.values()) {
-			System.out.printf(command.getHelpMessage());
-		}
-
 	}
 
 	// GENERAL METHODS
 
 	/**
 	 * Shows a warning message when the user enters an unknown command
+	 * and cleans any remainers (e.g.: arguments) of such command
 	 *
-	 * @param in - Scanner for running in.nextLine() and skipping any bad input
+	 * @param in {@link Scanner} that might contain bad input needing to be cleared
 	 */
-	private static void processUnknownCommand(Scanner in) {
-		System.out.println(UNKNOWN_COMMAND);
+	private static void unknownCommand(Scanner in) {
+		System.out.println(Commands.UNKNOWN.getWarningMessage());
 		in.nextLine();
 	}
 
