ccf645b05385bec227e2540e1f2f5a76c96ed7ff
jbordalo <j.bordalo@campus.fct.unl.pt>
2019-11-09 17:47:33 +0000

    Added removeTrip() to User and removed public modifier

--- src/fctBoleias/user/User.java
+++ src/fctBoleias/user/User.java
@@ -2,7 +2,10 @@ package fctBoleias.user;
 
 import java.io.Serializable;
 
+import basicDateTime.BasicDateTime;
+import fctBoleias.NoTripOnDayException;
 import fctBoleias.trip.Trip;
+import fctBoleias.trip.TripHasRidesException;
 
 public interface User extends Serializable {
 
@@ -10,25 +13,25 @@ public interface User extends Serializable {
 	 * Gives the {@link User}'s email
 	 * @return the email
 	 */
-	public String getEmail();
+	String getEmail();
 
 	/**
 	 * Gives the {@link User}'s name
 	 * @return the name
 	 */
-	public String getName();
+	String getName();
 
 	/**
 	 * Gives the {@link User}'s password
 	 * @return the password
 	 */
-	public String getPassword();
+	String getPassword();
 
 	/**
 	 * Gives the number of system visits from this {@link User} 
 	 * @return the nVisits
 	 */
-	public int getNumberLogins();
+	int getNumberLogins();
 	
 	/**
 	 * Matches the given {@link String} with the {@link User User's} password
@@ -36,22 +39,31 @@ public interface User extends Serializable {
 	 * @return <code>true</code> if the given {@link String} and the {@link User User's} password match,
 	 * <code>false</code> otherwise
 	 */
-	public boolean checkPassword(String password);
+	boolean checkPassword(String password);
 
 	/**
 	 * Registers a new login in the system by the {@link User}
 	 */
-	public void addLogin();
+	void addLogin();
 
 	/**
 	 * Registers a new trip on this {@link User}
 	 * @param newTrip {@link Trip} to be registered
 	 */
-	public void addTrip(Trip newTrip);
+	void addTrip(Trip newTrip);
 
 	/**
 	 * @return {@link User User's} number of {@link Trip Trips}
 	 */
-	public int getNumberTrips();
+	int getNumberTrips();
+
+	/**
+	 * Removes a {@link Trip trip} from this {@link User} on the given {@link BasicDateTime date}
+	 * @param date {@link BasicDateTime Date} of the {@link Trip} to be removed 
+	 * @return boolean true if the {@link Trip} was removed, false otherwise
+	 * @throws NoTripOnDayException if there's no {@link Trip} registers on the given {@link BasicDateTime date}
+	 * @throws TripHasRidesException if the {@link Trip} already has rides and can no longer be removed TODO
+	 */
+	boolean removeTrip(BasicDateTime date) throws NoTripOnDayException, TripHasRidesException;
 
 }
