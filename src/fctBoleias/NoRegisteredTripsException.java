928905fb93f0a882f0724d5cf86845a15d96987e
jbordalo <j.bordalo@campus.fct.unl.pt>
2019-11-10 00:54:12 +0000

    Added NoRegisteredTripsException

---
+++ src/fctBoleias/NoRegisteredTripsException.java
@@ -0,0 +1,18 @@
+package fctBoleias;
+
+public class NoRegisteredTripsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NoRegisteredTripsException() {
+		super();
+	}
+
+	public NoRegisteredTripsException(String message) {
+		super(message);
+	}
+
+}
