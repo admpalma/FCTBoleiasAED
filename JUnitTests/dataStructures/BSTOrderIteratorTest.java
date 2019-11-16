89f3948ae23ee091488d9e53dde3ea31ce732780
André Palma <andremarquespalma@gmail.com>
2019-11-16 21:21:46 +0000

    Add BSTOrderIterator Skeleton and Corresponding JUnit

---
+++ JUnitTests/dataStructures/BSTOrderIteratorTest.java
@@ -0,0 +1,67 @@
+package dataStructures;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import dataStructures.BST;
+import dataStructures.BSTOrderIterator;
+
+class BSTOrderIteratorTest {
+
+	private BST<Integer, Integer> bst;
+	private BSTOrderIterator<Integer, Integer> bstIt;
+	private BSTOrderIterator<Integer, Integer> bstItFail;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		bst = new BST<Integer, Integer>();
+		for (int i = 0; i < 20; i++) {
+			bst.insert(i, i);
+		}
+		bstIt = new BSTOrderIterator<Integer, Integer>(bst.root);
+		bstItFail = new BSTOrderIterator<Integer, Integer>(null);
+	}
+
+	@Test
+	void testBSTOrderIterator() {
+		assertFalse(bstItFail.hasNext());
+		assertThrows(NoSuchElementException.class, () -> bstItFail.next());
+		assertTrue(bstItFail.hasNext());
+		int i = 0;
+		while (bstIt.hasNext()) {
+			assertEquals(i++, bstIt.next());
+		}
+		assertThrows(NoSuchElementException.class, () -> bstItFail.next());
+	}
+
+	@Test
+	void testHasNext() {
+		assertFalse(bstItFail.hasNext());
+		for (int i = 0; i < 20; i++) {
+			assertTrue(bstItFail.hasNext());
+			bstIt.next();
+		}
+		assertThrows(NoSuchElementException.class, () -> bstItFail.next());
+	}
+
+	@Test
+	void testNext() {
+		bstItFail = new BSTOrderIterator<Integer, Integer>(null);
+		assertThrows(NoSuchElementException.class, () -> bstItFail.next());
+		int i = 0;
+		while (bstIt.hasNext()) {
+			assertEquals(i++, bstIt.next());
+		}
+		assertThrows(NoSuchElementException.class, () -> bstItFail.next());
+	}
+
+	@Test
+	void testRewind() {
+		bstIt.rewind();
+		testHasNext();
+		testNext();
+	}
+
+}
---
+++ src/dataStructures/BSTOrderIterator.java
@@ -0,0 +1,29 @@
+package dataStructures;
+
+import dataStructures.BST.BSTNode;
+
+public class BSTOrderIterator<K, V> implements Iterator<Entry<K, V>> {
+
+	public BSTOrderIterator(BSTNode<Entry<K, V>> root) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean hasNext() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Entry<K, V> next() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void rewind() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
