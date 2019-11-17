172a307c10de0010f8f7d465c2ae316cd27ae8b0
André Palma <andremarquespalma@gmail.com>
2019-11-17 19:01:59 +0000

    Optimize listCurrentUserRides() and printPrompt()

--- src/fctBoleias/trip/TripWrapper.java
+++ src/fctBoleias/trip/TripWrapper.java
@@ -67,11 +67,20 @@ public interface TripWrapper extends Wrapper {
 	 * {@link #getOrigin() origin}-{@link #getDestination() destination}<br>
 	 * {@link BasicDateTime#toString() dateTime} {@link #getDuration() duration}<br>
 	 * <code>Lugares vagos: </code>{@link #freeSlots() free slots}<br>
-	 * <code>"Users in trip emails"</code><br>
+	 * <code>"Users in trip emails'"</code><br>
 	 * <code>Em espera: "number of users in queue"</code><br>
 	 * @return {@link String} describing the {@link Trip}
 	 */
 	String toString();
+	
+	/**
+	 * Returns a {@link String} with the following format:<p>
+	 * {@link #getDriverEmail() driver email}<br>
+	 * {@link #getOrigin() origin}-{@link #getDestination() destination}<br>
+	 * {@link BasicDateTime#toString() dateTime} {@link #getDuration() duration}<br>
+	 * @return {@link String} describing the {@link Trip} which is being taken as a ride
+	 */
+	String toRideString();
 
 	/**
 	 * @return the number of free slots in this {@link Trip}
